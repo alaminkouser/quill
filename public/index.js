a92418e847618dfe7600fa0115b6b87a9dac2a06
AL AMIN KOUSER <alaminkouser@icloud.com>
2024-02-24 15:46:00 +0600

    fixed: css

--- public/index.js
+++ public/index.js
@@ -6,6 +6,8 @@ let DATA = null;
 const createButton = document.querySelector("#createButton");
 const loadButton = document.querySelector("#loadButton");
 const dialog = document.querySelector("dialog");
+const quillTextarea = document.querySelector("#quillTextarea");
+const discardQuill = document.querySelector("#discardQuill");
 
 
 createButton.addEventListener("click", (_) => {
@@ -20,5 +22,21 @@ createButton.addEventListener("click", (_) => {
 });
 
 newQuill.addEventListener("click", (_) => {
-  dialog.open = true;
+  dialog.showModal();
+});
+
+function autoIncreaseHeight(textarea) {
+  console.log(textarea);
+  textarea.style.height = "auto";
+  console.log(Math.min(textarea.scrollHeight, parseInt(window.getComputedStyle(textarea).maxHeight)));
+  textarea.style.height = Math.min(textarea.scrollHeight, parseInt(window.getComputedStyle(textarea).maxHeight)) + "px";
+}
+
+quillTextarea.oninput = () => {
+  autoIncreaseHeight(quillTextarea);
+  console.log(true);
+}
+
+discardQuill.addEventListener("click", (_) => {
+  dialog.close();
 });
